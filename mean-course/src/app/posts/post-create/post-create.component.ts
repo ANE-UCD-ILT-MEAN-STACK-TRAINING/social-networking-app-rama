73c20fc5de8777dc8315e10bb4c95a615a9b7c49
rama842 <srama777@gmail.com>
2020-05-05 14:03:41 -0400

    Initial version upto Template binding

---
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+ /* onAddPost(){
+    alert(" Save Post Button Clicked");
+  }*/
+
+  newPost = 'No Content';
+  onAddPost(postInput : HTMLTextAreaElement){
+    console.dir(postInput);
+    this.newPost= postInput.value;
+}
+
+
+}
