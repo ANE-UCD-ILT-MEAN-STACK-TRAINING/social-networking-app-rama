ada6301285c5048495cca74867d68cdafa1802f2
rama842 <srama777@gmail.com>
2020-05-07 17:28:09 -0400

    PostModel on top of decorator.

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component,  OnInit, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create',
@@ -27,7 +28,7 @@ export class PostCreateComponent implements OnInit {
 enteredTitle = '';
 enteredContent = '';
 onAddPost() {   
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content:this.enteredContent
     }
